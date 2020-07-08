80be54d3ddf6e12a29ada8e75daea8ff35a9b3e6
shubhamchaudhary <shubham.chaudhary@mayadata.io>
2020-07-08 16:49:33 +0530

    feat(node-cpu-hog): Adding the node-cpu-hog experiment
    Signed-off-by: shubhamchaudhary <shubham.chaudhary@mayadata.io>

---
+++ pkg/generic/node-cpu-hog/types/types.go
@@ -0,0 +1,25 @@
+package types
+
+import (
+	clientTypes "k8s.io/apimachinery/pkg/types"
+)
+
+// ExperimentDetails is for collecting all the experiment-related details
+type ExperimentDetails struct {
+	ExperimentName   string
+	EngineName       string
+	ChaosDuration    int
+	RampTime         int
+	ChaosLib         string
+	AppNS            string
+	AppLabel         string
+	AppKind          string
+	ChaosUID         clientTypes.UID
+	InstanceID       string
+	ChaosNamespace   string
+	ChaosPodName     string
+	NodeCPUcores     int
+	RunID            string
+	LIBImage         string
+	AuxiliaryAppInfo string
+}
