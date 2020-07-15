a4a3198b12b0cfc4c301ef0fc130aba10738a305
Shubham Chaudhary <shubham.chaudhary@mayadata.io>
2020-07-15 18:17:06 +0530

    feat(go-experiments): Updating failure condition for helper pod (#57)
    Signed-off-by: shubhamchaudhary <shubham.chaudhary@mayadata.io>

--- chaoslib/litmus/node_cpu_hog/node-cpu-hog.go
+++ chaoslib/litmus/node_cpu_hog/node-cpu-hog.go
@@ -72,9 +72,9 @@ func PrepareNodeCPUHog(experimentsDetails *experimentTypes.ExperimentDetails, cl
 	// Wait till the completion of helper pod
 	log.Infof("[Wait]: Waiting for %vs till the completion of the helper pod", strconv.Itoa(experimentsDetails.ChaosDuration+30))
 
-	err = status.WaitForCompletion(experimentsDetails.ChaosNamespace, "name=node-cpu-hog-"+experimentsDetails.RunID, clients, experimentsDetails.ChaosDuration+30)
-	if err != nil {
-		return err
+	podStatus, err := status.WaitForCompletion(experimentsDetails.ChaosNamespace, "name=node-cpu-hog-"+experimentsDetails.RunID, clients, experimentsDetails.ChaosDuration+30)
+	if err != nil || podStatus == "Failed" {
+		return errors.Errorf("helper pod failed due to, err: %v", err)
 	}
 
 	// Checking the status of application node
