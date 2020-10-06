2a263e376b966bf2b095d4544385bbce4115375c
Shubham Chaudhary <shubham.chaudhary@mayadata.io>
2020-10-06 11:46:01 +0530

    chore(skip-chaos-pods): ignore chaos-pod to be target if applabel in empty (#164)
    Signed-off-by: shubhamchaudhary <shubham.chaudhary@mayadata.io>

--- chaoslib/pumba/cpu-chaos/lib/cpu-chaos.go
+++ chaoslib/pumba/cpu-chaos/lib/cpu-chaos.go
@@ -21,7 +21,7 @@ var err error
 // PreparePodCPUHog contains prepration steps before chaos injection
 func PreparePodCPUHog(experimentsDetails *experimentTypes.ExperimentDetails, clients clients.ClientSets, resultDetails *types.ResultDetails, eventsDetails *types.EventDetails, chaosDetails *types.ChaosDetails) error {
 
-	targetPodList, err := common.GetPodList(experimentsDetails.AppNS, experimentsDetails.TargetPod, experimentsDetails.AppLabel, experimentsDetails.PodsAffectedPerc, clients)
+	targetPodList, err := common.GetPodList(experimentsDetails.AppNS, experimentsDetails.TargetPod, experimentsDetails.AppLabel, string(experimentsDetails.ChaosUID), experimentsDetails.PodsAffectedPerc, clients)
 	if err != nil {
 		return err
 	}
--- chaoslib/pumba/memory-chaos/lib/memory-chaos.go
+++ chaoslib/pumba/memory-chaos/lib/memory-chaos.go
@@ -21,7 +21,7 @@ var err error
 // PreparePodMemoryHog contains prepration steps before chaos injection
 func PreparePodMemoryHog(experimentsDetails *experimentTypes.ExperimentDetails, clients clients.ClientSets, resultDetails *types.ResultDetails, eventsDetails *types.EventDetails, chaosDetails *types.ChaosDetails) error {
 
-	targetPodList, err := common.GetPodList(experimentsDetails.AppNS, experimentsDetails.TargetPod, experimentsDetails.AppLabel, experimentsDetails.PodsAffectedPerc, clients)
+	targetPodList, err := common.GetPodList(experimentsDetails.AppNS, experimentsDetails.TargetPod, experimentsDetails.AppLabel, string(experimentsDetails.ChaosUID), experimentsDetails.PodsAffectedPerc, clients)
 	if err != nil {
 		return err
 	}
