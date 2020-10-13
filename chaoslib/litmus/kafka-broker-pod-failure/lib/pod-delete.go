b60de824d2db5ee1da5d8858934c73765775d113
UDIT GAURAV <35391335+uditgaurav@users.noreply.github.com>
2020-10-13 14:31:40 +0530

    chore(kafka): Add kafka broker pod failure experiment in litmus-go (#93)
    * chore(kafka): Add kafka broker pod failure experiment in litmus-go
    
    Signed-off-by: Udit Gaurav <uditgaurav@gmail.com>
    
    * chore(goExp): Update kafka experiment
    
    Signed-off-by: Udit Gaurav <udit.gaurav@mayadata.io>

---
+++ chaoslib/litmus/kafka-broker-pod-failure/lib/pod-delete.go
@@ -0,0 +1,204 @@
+package lib
+
+import (
+	"time"
+
+	pod_delete "github.com/litmuschaos/litmus-go/chaoslib/litmus/pod-delete/lib"
+	clients "github.com/litmuschaos/litmus-go/pkg/clients"
+	"github.com/litmuschaos/litmus-go/pkg/events"
+	experimentTypes "github.com/litmuschaos/litmus-go/pkg/generic/pod-delete/types"
+	"github.com/litmuschaos/litmus-go/pkg/kafka"
+	kafkaTypes "github.com/litmuschaos/litmus-go/pkg/kafka/types"
+	"github.com/litmuschaos/litmus-go/pkg/log"
+	"github.com/litmuschaos/litmus-go/pkg/status"
+	"github.com/litmuschaos/litmus-go/pkg/types"
+	"github.com/litmuschaos/litmus-go/pkg/utils/common"
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+var err error
+
+//PreparePodDelete contains the prepration steps before chaos injection
+func PreparePodDelete(kafkaDetails *kafkaTypes.ExperimentDetails, experimentsDetails *experimentTypes.ExperimentDetails, clients clients.ClientSets, resultDetails *types.ResultDetails, eventsDetails *types.EventDetails, chaosDetails *types.ChaosDetails) error {
+
+	//Getting the iteration count for the pod deletion
+	pod_delete.GetIterations(experimentsDetails)
+
+	//Waiting for the ramp time before chaos injection
+	if experimentsDetails.RampTime != 0 {
+		log.Infof("[Ramp]: Waiting for the %vs ramp time before injecting chaos", experimentsDetails.RampTime)
+		common.WaitForDuration(experimentsDetails.RampTime)
+	}
+
+	if experimentsDetails.Sequence == "serial" {
+		if err = InjectChaosInSerialMode(kafkaDetails, experimentsDetails, clients, chaosDetails, eventsDetails); err != nil {
+			return err
+		}
+	} else {
+		if err = InjectChaosInParallelMode(kafkaDetails, experimentsDetails, clients, chaosDetails, eventsDetails); err != nil {
+			return err
+		}
+	}
+
+	//Waiting for the ramp time after chaos injection
+	if experimentsDetails.RampTime != 0 {
+		log.Infof("[Ramp]: Waiting for the %vs ramp time after injecting chaos", experimentsDetails.RampTime)
+		common.WaitForDuration(experimentsDetails.RampTime)
+	}
+	return nil
+}
+
+// InjectChaosInSerialMode kill the container of all target application serially (one by one)
+func InjectChaosInSerialMode(kafkaDetails *kafkaTypes.ExperimentDetails, experimentsDetails *experimentTypes.ExperimentDetails, clients clients.ClientSets, chaosDetails *types.ChaosDetails, eventsDetails *types.EventDetails) error {
+
+	GracePeriod := int64(0)
+	//ChaosStartTimeStamp contains the start timestamp, when the chaos injection begin
+	ChaosStartTimeStamp := time.Now().Unix()
+
+	for count := 0; count < experimentsDetails.Iterations; count++ {
+
+		//When broker is not defined
+		if kafkaDetails.ChaoslibDetail.TargetPod == "" {
+			err = kafka.LaunchStreamDeriveLeader(kafkaDetails, clients)
+			if err != nil {
+				return errors.Errorf("fail to derive the leader err: %v", err)
+			}
+		}
+
+		// Get the target pod details for the chaos execution
+		// if the target pod is not defined it will derive the random target pod list using pod affected percentage
+		targetPodList, err := common.GetPodList(experimentsDetails.AppNS, experimentsDetails.TargetPod, experimentsDetails.AppLabel, string(experimentsDetails.ChaosUID), experimentsDetails.PodsAffectedPerc, clients)
+		if err != nil {
+			return err
+		}
+
+		if experimentsDetails.EngineName != "" {
+			msg := "Injecting " + experimentsDetails.ExperimentName + " chaos on application pod"
+			types.SetEngineEventAttributes(eventsDetails, types.ChaosInject, msg, "Normal", chaosDetails)
+			events.GenerateEvents(eventsDetails, clients, chaosDetails, "ChaosEngine")
+		}
+
+		//Deleting the application pod
+		for _, pod := range targetPodList.Items {
+
+			log.InfoWithValues("[Info]: Killing the following pods", logrus.Fields{
+				"PodName": pod.Name})
+
+			if experimentsDetails.Force == true {
+				err = clients.KubeClient.CoreV1().Pods(experimentsDetails.AppNS).Delete(pod.Name, &v1.DeleteOptions{GracePeriodSeconds: &GracePeriod})
+			} else {
+				err = clients.KubeClient.CoreV1().Pods(experimentsDetails.AppNS).Delete(pod.Name, &v1.DeleteOptions{})
+			}
+			if err != nil {
+				return err
+			}
+
+			//Waiting for the chaos interval after chaos injection
+			if experimentsDetails.ChaosInterval != 0 {
+				log.Infof("[Wait]: Wait for the chaos interval %vs", experimentsDetails.ChaosInterval)
+				common.WaitForDuration(experimentsDetails.ChaosInterval)
+			}
+
+			//Verify the status of pod after the chaos injection
+			log.Info("[Status]: Verification for the recreation of application pod")
+			if err = status.CheckApplicationStatus(experimentsDetails.AppNS, experimentsDetails.AppLabel, experimentsDetails.Timeout, experimentsDetails.Delay, clients); err != nil {
+				return err
+			}
+
+			//ChaosCurrentTimeStamp contains the current timestamp
+			ChaosCurrentTimeStamp := time.Now().Unix()
+
+			//ChaosDiffTimeStamp contains the difference of current timestamp and start timestamp
+			//It will helpful to track the total chaos duration
+			chaosDiffTimeStamp := ChaosCurrentTimeStamp - ChaosStartTimeStamp
+
+			if int(chaosDiffTimeStamp) >= experimentsDetails.ChaosDuration {
+				log.Infof("[Chaos]: Time is up for experiment: %v", experimentsDetails.ExperimentName)
+				break
+			}
+		}
+
+	}
+	log.Infof("[Completion]: %v chaos is done", experimentsDetails.ExperimentName)
+
+	return nil
+
+}
+
+// InjectChaosInParallelMode kill the container of all target application in parallel mode (all at once)
+func InjectChaosInParallelMode(kafkaDetails *kafkaTypes.ExperimentDetails, experimentsDetails *experimentTypes.ExperimentDetails, clients clients.ClientSets, chaosDetails *types.ChaosDetails, eventsDetails *types.EventDetails) error {
+
+	GracePeriod := int64(0)
+	//ChaosStartTimeStamp contains the start timestamp, when the chaos injection begin
+	ChaosStartTimeStamp := time.Now().Unix()
+
+	for count := 0; count < experimentsDetails.Iterations; count++ {
+
+		//When broker is not defined
+		if kafkaDetails.ChaoslibDetail.TargetPod == "" {
+			err = kafka.LaunchStreamDeriveLeader(kafkaDetails, clients)
+			if err != nil {
+				return errors.Errorf("fail to derive the leader err: %v", err)
+			}
+		}
+
+		// Get the target pod details for the chaos execution
+		// if the target pod is not defined it will derive the random target pod list using pod affected percentage
+		targetPodList, err := common.GetPodList(experimentsDetails.AppNS, experimentsDetails.TargetPod, experimentsDetails.AppLabel, string(experimentsDetails.ChaosUID), experimentsDetails.PodsAffectedPerc, clients)
+		if err != nil {
+			return err
+		}
+
+		if experimentsDetails.EngineName != "" {
+			msg := "Injecting " + experimentsDetails.ExperimentName + " chaos on application pod"
+			types.SetEngineEventAttributes(eventsDetails, types.ChaosInject, msg, "Normal", chaosDetails)
+			events.GenerateEvents(eventsDetails, clients, chaosDetails, "ChaosEngine")
+		}
+
+		//Deleting the application pod
+		for _, pod := range targetPodList.Items {
+
+			log.InfoWithValues("[Info]: Killing the following pods", logrus.Fields{
+				"PodName": pod.Name})
+
+			if experimentsDetails.Force == true {
+				err = clients.KubeClient.CoreV1().Pods(experimentsDetails.AppNS).Delete(pod.Name, &v1.DeleteOptions{GracePeriodSeconds: &GracePeriod})
+			} else {
+				err = clients.KubeClient.CoreV1().Pods(experimentsDetails.AppNS).Delete(pod.Name, &v1.DeleteOptions{})
+			}
+			if err != nil {
+				return err
+			}
+		}
+
+		//Waiting for the chaos interval after chaos injection
+		if experimentsDetails.ChaosInterval != 0 {
+			log.Infof("[Wait]: Wait for the chaos interval %vs", experimentsDetails.ChaosInterval)
+			common.WaitForDuration(experimentsDetails.ChaosInterval)
+		}
+
+		//Verify the status of pod after the chaos injection
+		log.Info("[Status]: Verification for the recreation of application pod")
+		if err = status.CheckApplicationStatus(experimentsDetails.AppNS, experimentsDetails.AppLabel, experimentsDetails.Timeout, experimentsDetails.Delay, clients); err != nil {
+			return err
+		}
+
+		//ChaosCurrentTimeStamp contains the current timestamp
+		ChaosCurrentTimeStamp := time.Now().Unix()
+
+		//ChaosDiffTimeStamp contains the difference of current timestamp and start timestamp
+		//It will helpful to track the total chaos duration
+		chaosDiffTimeStamp := ChaosCurrentTimeStamp - ChaosStartTimeStamp
+
+		if int(chaosDiffTimeStamp) >= experimentsDetails.ChaosDuration {
+			log.Infof("[Chaos]: Time is up for experiment: %v", experimentsDetails.ExperimentName)
+			break
+		}
+	}
+
+	log.Infof("[Completion]: %v chaos is done", experimentsDetails.ExperimentName)
+
+	return nil
+}
---
+++ experiments/kafka/kafka-broker-pod-failure/experiment/kafka-broker-pod-failure.go
@@ -0,0 +1,201 @@
+package experiment
+
+import (
+	kafka_broker_pod_failure "github.com/litmuschaos/litmus-go/chaoslib/litmus/kafka-broker-pod-failure/lib"
+	clients "github.com/litmuschaos/litmus-go/pkg/clients"
+	"github.com/litmuschaos/litmus-go/pkg/events"
+	"github.com/litmuschaos/litmus-go/pkg/kafka"
+	experimentEnv "github.com/litmuschaos/litmus-go/pkg/kafka/environment"
+	experimentTypes "github.com/litmuschaos/litmus-go/pkg/kafka/types"
+	"github.com/litmuschaos/litmus-go/pkg/log"
+	"github.com/litmuschaos/litmus-go/pkg/result"
+	"github.com/litmuschaos/litmus-go/pkg/status"
+	"github.com/litmuschaos/litmus-go/pkg/types"
+	"github.com/sirupsen/logrus"
+)
+
+// KafkaBrokerPodFailure inject the kafka-broker-pod-failure chaos
+func KafkaBrokerPodFailure(clients clients.ClientSets) {
+
+	var err error
+	experimentsDetails := experimentTypes.ExperimentDetails{}
+	resultDetails := types.ResultDetails{}
+	eventsDetails := types.EventDetails{}
+	chaosDetails := types.ChaosDetails{}
+
+	//Fetching all the ENV passed from the runner pod
+	log.Info("[PreReq]: Getting the ENV for the kafka-broker-pod-failure")
+	experimentEnv.GetENV(&experimentsDetails)
+
+	// Intialise the chaos attributes
+	experimentEnv.InitialiseChaosVariables(&chaosDetails, &experimentsDetails)
+
+	// Intialise Chaos Result Parameters
+	types.SetResultAttributes(&resultDetails, chaosDetails)
+
+	//Updating the chaos result in the beginning of experiment
+	log.Infof("[PreReq]: Updating the chaos result of %v experiment (SOT)", experimentsDetails.ChaoslibDetail.ExperimentName)
+	err = result.ChaosResult(&chaosDetails, clients, &resultDetails, "SOT")
+	if err != nil {
+		log.Errorf("Unable to Create the Chaos Result err: %v", err)
+		failStep := "Updating the chaos result of kafka-broker-pod-failure experiment (SOT)"
+		types.SetResultAfterCompletion(&resultDetails, "Fail", "Completed", failStep)
+		err = result.ChaosResult(&chaosDetails, clients, &resultDetails, "EOT")
+		return
+	}
+
+	// Set the chaos result uid
+	result.SetResultUID(&resultDetails, clients, &chaosDetails)
+
+	// generating the event in chaosresult to marked the verdict as awaited
+	msg := "experiment: " + experimentsDetails.ChaoslibDetail.ExperimentName + ", Result: Awaited"
+	types.SetResultEventAttributes(&eventsDetails, types.AwaitedVerdict, msg, "Normal", &resultDetails)
+	events.GenerateEvents(&eventsDetails, clients, &chaosDetails, "ChaosResult")
+
+	//DISPLAY THE APP INFORMATION
+	log.InfoWithValues("The application informations are as follows", logrus.Fields{
+		"Kafka Namespace": experimentsDetails.KafkaNamespace,
+		"Kafka Label":     experimentsDetails.KafkaLabel,
+		"Ramp Time":       experimentsDetails.ChaoslibDetail.RampTime,
+	})
+
+	// PRE-CHAOS APPLICATION STATUS CHECK
+	// KAFKA CLUSTER HEALTH CHECK
+	log.Info("[Status]: Verify that the Kafka cluster is healthy(pre-chaos)")
+	err = kafka.ClusterHealthCheck(&experimentsDetails, clients)
+	if err != nil {
+		log.Errorf("Cluster status check failed err: %v", err)
+		failStep := "Verify that the AUT (Application Under Test) is running (pre-chaos)"
+		types.SetResultAfterCompletion(&resultDetails, "Fail", "Completed", failStep)
+		result.ChaosResult(&chaosDetails, clients, &resultDetails, "EOT")
+		return
+	}
+	if experimentsDetails.ChaoslibDetail.EngineName != "" {
+		types.SetEngineEventAttributes(&eventsDetails, types.PreChaosCheck, "AUT is Running successfully", "Normal", &chaosDetails)
+		events.GenerateEvents(&eventsDetails, clients, &chaosDetails, "ChaosEngine")
+	}
+
+	// PRE-CHAOS KAFKA APPLICATION LIVENESS CHECK
+	// when the kafka broker is provided
+	if experimentsDetails.KafkaBroker != "" {
+		if experimentsDetails.KafkaLivenessStream == "enabled" {
+			_, err := kafka.LivenessStream(&experimentsDetails, clients)
+			if err != nil {
+				log.Errorf("Liveness check failed err: %v", err)
+				failStep := "Verify liveness check (pre-chaos)"
+				types.SetResultAfterCompletion(&resultDetails, "Fail", "Completed", failStep)
+				result.ChaosResult(&chaosDetails, clients, &resultDetails, "EOT")
+				return
+
+			}
+			log.Info("The Liveness pod gets established")
+
+		} else if experimentsDetails.KafkaLivenessStream == "" || experimentsDetails.KafkaLivenessStream == "disabled" {
+			kafka.DisplayKafkaBroker(&experimentsDetails)
+		}
+	}
+
+	// when the kafka broker is not provided
+	if experimentsDetails.KafkaBroker == "" {
+		if experimentsDetails.KafkaLivenessStream == "enabled" {
+			err = kafka.LaunchStreamDeriveLeader(&experimentsDetails, clients)
+			if err != nil {
+				log.Errorf("Error: %v", err)
+				failStep := "Launch the stream derive leader"
+				types.SetResultAfterCompletion(&resultDetails, "Fail", "Completed", failStep)
+				result.ChaosResult(&chaosDetails, clients, &resultDetails, "EOT")
+				return
+			}
+		} else if experimentsDetails.KafkaLivenessStream == "" || experimentsDetails.KafkaLivenessStream == "disabled" {
+			_, err := kafka.SelectBroker(&experimentsDetails, "", clients)
+			if err != nil {
+				log.Errorf("Error: %v", err)
+				failStep := "Selecting broker when liveness is disabled"
+				types.SetResultAfterCompletion(&resultDetails, "Fail", "Completed", failStep)
+				result.ChaosResult(&chaosDetails, clients, &resultDetails, "EOT")
+				return
+			}
+		}
+	}
+
+	// Including the litmus lib for kafka-broker-pod-failure
+	if experimentsDetails.ChaoslibDetail.ChaosLib == "litmus" {
+		err = kafka_broker_pod_failure.PreparePodDelete(&experimentsDetails, experimentsDetails.ChaoslibDetail, clients, &resultDetails, &eventsDetails, &chaosDetails)
+		if err != nil {
+			log.Errorf("Chaos injection failed err: %v", err)
+			failStep := "Including the litmus lib for kafka-broker-pod-failure"
+			types.SetResultAfterCompletion(&resultDetails, "Fail", "Completed", failStep)
+			result.ChaosResult(&chaosDetails, clients, &resultDetails, "EOT")
+			return
+		}
+		log.Info("[Confirmation]: The application pod has been deleted successfully")
+		resultDetails.Verdict = "Pass"
+	} else {
+		log.Error("[Invalid]: Please Provide the correct LIB")
+		failStep := "Including the litmus lib for kafka-broker-pod-failure"
+		types.SetResultAfterCompletion(&resultDetails, "Fail", "Completed", failStep)
+		result.ChaosResult(&chaosDetails, clients, &resultDetails, "EOT")
+		return
+	}
+
+	// POST-CHAOS KAFKA CLUSTER HEALTH CHECK
+	log.Info("[Status]: Verify that the Kafka cluster is healthy(post-chaos)")
+	err = kafka.ClusterHealthCheck(&experimentsDetails, clients)
+	if err != nil {
+		log.Errorf("Cluster status check failederr: %v", err)
+		failStep := "Verify that the AUT (Application Under Test) is running (pre-chaos)"
+		types.SetResultAfterCompletion(&resultDetails, "Fail", "Completed", failStep)
+		result.ChaosResult(&chaosDetails, clients, &resultDetails, "EOT")
+		return
+	}
+	if experimentsDetails.ChaoslibDetail.EngineName != "" {
+		types.SetEngineEventAttributes(&eventsDetails, types.PreChaosCheck, "AUT is Running successfully", "Normal", &chaosDetails)
+		events.GenerateEvents(&eventsDetails, clients, &chaosDetails, "ChaosEngine")
+	}
+
+	// Liveness Status Check (post-chaos) and cleanup
+	if experimentsDetails.KafkaLivenessStream != "" {
+		err = status.CheckApplicationStatus(experimentsDetails.ChaoslibDetail.AppNS, "name=kafka-liveness", experimentsDetails.ChaoslibDetail.Timeout, experimentsDetails.ChaoslibDetail.Delay, clients)
+		if err != nil {
+			log.Errorf("Application liveness check failed err: %v", err)
+			failStep := "Verify that the AUT (Application Under Test) is running (post-chaos)"
+			types.SetResultAfterCompletion(&resultDetails, "Fail", "Completed", failStep)
+			result.ChaosResult(&chaosDetails, clients, &resultDetails, "EOT")
+			return
+		}
+
+		if err := kafka.LivenessCleanup(&experimentsDetails, clients); err != nil {
+			log.Errorf("liveness cleanup failed err: %v", err)
+			failStep := "Performing cleanup post chaos"
+			types.SetResultAfterCompletion(&resultDetails, "Fail", "Completed", failStep)
+			result.ChaosResult(&chaosDetails, clients, &resultDetails, "EOT")
+			return
+		}
+
+	}
+
+	//Updating the chaosResult in the end of experiment
+	log.Info("[The End]: Updating the chaos result of kafka pod delete experiment (EOT)")
+	err = result.ChaosResult(&chaosDetails, clients, &resultDetails, "EOT")
+	if err != nil {
+		log.Errorf("Unable to Update the Chaos Result err: %v", err)
+	}
+
+	// generating the event in chaosresult to marked the verdict as pass/fail
+	msg = "experiment: " + experimentsDetails.ChaoslibDetail.ExperimentName + ", Result: " + resultDetails.Verdict
+	reason := types.PassVerdict
+	eventType := "Normal"
+	if resultDetails.Verdict != "Pass" {
+		reason = types.FailVerdict
+		eventType = "Warning"
+	}
+
+	types.SetResultEventAttributes(&eventsDetails, reason, msg, eventType, &resultDetails)
+	events.GenerateEvents(&eventsDetails, clients, &chaosDetails, "ChaosResult")
+
+	if experimentsDetails.ChaoslibDetail.EngineName != "" {
+		msg := experimentsDetails.ChaoslibDetail.ExperimentName + " experiment has been " + resultDetails.Verdict + "ed"
+		types.SetEngineEventAttributes(&eventsDetails, types.Summary, msg, "Normal", &chaosDetails)
+		events.GenerateEvents(&eventsDetails, clients, &chaosDetails, "ChaosEngine")
+	}
+}
