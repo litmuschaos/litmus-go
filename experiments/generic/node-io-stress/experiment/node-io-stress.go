60a82eeccda73b239cbc266321e68e299476ad78
shubhamchaudhary <shubham.chaudhary@mayadata.io>
2020-10-15 14:48:55 +0530

    refactor(experiment): covered uncaught errors
    Signed-off-by: shubhamchaudhary <shubham.chaudhary@mayadata.io>

--- experiments/generic/node-io-stress/experiment/node-io-stress.go
+++ experiments/generic/node-io-stress/experiment/node-io-stress.go
@@ -35,8 +35,10 @@ func NodeIOStress(clients clients.ClientSets) {
 	// Intialise Chaos Result Parameters
 	types.SetResultAttributes(&resultDetails, chaosDetails)
 
-	// Intialise the probe details
-	probe.InitializeProbesInChaosResultDetails(&chaosDetails, clients, &resultDetails)
+	// Intialise the probe details. Bail out upon error, as we haven't entered exp business logic yet
+	if err = probe.InitializeProbesInChaosResultDetails(&chaosDetails, clients, &resultDetails); err != nil {
+		log.Fatalf("Unable to initialize the probes, err: %v", err)
+	}
 
 	//Updating the chaos result in the beginning of experiment
 	log.Infof("[PreReq]: Updating the chaos result of %v experiment (SOT)", experimentsDetails.ExperimentName)
