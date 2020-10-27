81ecfb5b8cf5323b744f2662e8c67c16c0d8a799
Udit Gaurav <udit.gaurav@mayadata.io>
2020-10-27 18:31:25 +0530

    chore(Exp): Add AWS EC2 terminate experiment
    Signed-off-by: Udit Gaurav <udit.gaurav@mayadata.io>

--- bin/go-runner.go
+++ bin/go-runner.go
@@ -21,6 +21,7 @@ import (
 	nodeIOStress "github.com/litmuschaos/litmus-go/experiments/generic/node-io-stress/experiment"
 	nodeMemoryHog "github.com/litmuschaos/litmus-go/experiments/generic/node-memory-hog/experiment"
 	nodeTaint "github.com/litmuschaos/litmus-go/experiments/generic/node-taint/experiment"
+	ec2terminate "github.com/litmuschaos/litmus-go/experiments/generic/platform/aws/ec2-terminate/experiment"
 	podAutoscaler "github.com/litmuschaos/litmus-go/experiments/generic/pod-autoscaler/experiment"
 	podCPUHog "github.com/litmuschaos/litmus-go/experiments/generic/pod-cpu-hog/experiment"
 	podDelete "github.com/litmuschaos/litmus-go/experiments/generic/pod-delete/experiment"
@@ -100,6 +101,8 @@ func main() {
 		podNetworkLoss.PodNetworkLoss(clients)
 	case "cassandra-pod-delete":
 		cassandraPodDelete.CasssandraPodDelete(clients)
+	case "ec2-terminate":
+		ec2terminate.EC2Terminate(clients)
 	default:
 		log.Fatalf("Unsupported -name %v, please provide the correct value of -name args", *experimentName)
 	}
---
+++ pkg/cloud/aws/ec2-instance-status.go
@@ -0,0 +1,47 @@
+package aws
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ec2"
+	experimentTypes "github.com/litmuschaos/litmus-go/pkg/generic/ec2-terminate/types"
+	"github.com/pkg/errors"
+)
+
+//GetEC2InstanceStatus will verify and give the ec2 instance details along with ebs volume idetails.
+func GetEC2InstanceStatus(experimentsDetails *experimentTypes.ExperimentDetails) (string, error) {
+
+	var isFound bool
+	// Load session from shared config
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+		Config:            aws.Config{Region: aws.String(experimentsDetails.Region)},
+	}))
+
+	// Create new EC2 client
+	ec2Svc := ec2.New(sess)
+
+	// Call to get detailed information on each instance
+	result, err := ec2Svc.DescribeInstances(nil)
+	if err != nil {
+		return "", err
+	}
+
+	isFound = false
+	for _, reservationDetails := range result.Reservations {
+
+		for _, instanceDetails := range reservationDetails.Instances {
+
+			if *instanceDetails.InstanceId == experimentsDetails.Ec2InstanceID {
+				isFound = true
+				return *instanceDetails.State.Name, nil
+			}
+		}
+	}
+
+	if isFound == false {
+		return "", errors.Errorf("fail to get the status of ec2 instance with instanceID %v", experimentsDetails.Ec2InstanceID)
+	}
+
+	return "", nil
+}
