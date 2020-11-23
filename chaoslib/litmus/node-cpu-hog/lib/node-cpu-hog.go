073a80e3037444874a0c85e951e179ddd6caf2ff
Shubham Chaudhary <shubham.chaudhary@mayadata.io>
2020-11-23 14:10:54 +0530

    Update node-cpu-hog.go

--- chaoslib/litmus/node-cpu-hog/lib/node-cpu-hog.go
+++ chaoslib/litmus/node-cpu-hog/lib/node-cpu-hog.go
@@ -239,7 +239,7 @@ func CreateHelperPod(experimentsDetails *experimentTypes.ExperimentDetails, appN
 				{
 					Name:            experimentsDetails.ExperimentName,
 					Image:           experimentsDetails.LIBImage,
-					ImagePullPolicy: experimentsDetails.LIBImagePullPolicy,
+					ImagePullPolicy: apiv1.PullPolicy(experimentsDetails.LIBImagePullPolicy),
 					Command: []string{
 						"stress-ng",
 					},
