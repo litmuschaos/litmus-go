08575a8e443ea0370b6f7b6835d282dd683b1abb
shubhamchaudhary <shubham.chaudhary@mayadata.io>
2020-12-10 20:45:22 +0530

    chore(promProbe): Adding queryPath in prom probe
    Signed-off-by: shubhamchaudhary <shubham.chaudhary@mayadata.io>

--- go.mod
+++ go.mod
@@ -11,7 +11,7 @@ require (
 	github.com/imdario/mergo v0.3.9 // indirect
 	github.com/kr/pretty v0.2.0 // indirect
 	github.com/kyokomi/emoji v2.2.4+incompatible
-	github.com/litmuschaos/chaos-operator v0.0.0-20201202182158-7c71b7d70dff
+	github.com/litmuschaos/chaos-operator v0.0.0-20201210172142-57fddee6734e
 	github.com/mailru/easyjson v0.7.1 // indirect
 	github.com/openebs/maya v0.0.0-20200411140727-1c81f9e017b0
 	github.com/pkg/errors v0.9.1
@@ -19,7 +19,6 @@ require (
 	github.com/sirupsen/logrus v1.6.0
 	github.com/spf13/cobra v1.0.0
 	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d // indirect
-	golang.org/x/sys v0.0.0-20200509044756-6aff5f38e54f // indirect
 	golang.org/x/time v0.0.0-20200416051211-89c76fbcd5d1 // indirect
 	google.golang.org/appengine v1.6.6 // indirect
 	google.golang.org/protobuf v1.24.0 // indirect
--- vendor/github.com/litmuschaos/chaos-operator/pkg/apis/litmuschaos/v1alpha1/chaosengine_types.go
+++ vendor/github.com/litmuschaos/chaos-operator/pkg/apis/litmuschaos/v1alpha1/chaosengine_types.go
@@ -149,6 +149,8 @@ type RunnerInfo struct {
 	Secrets []Secret `json:"secrets,omitempty"`
 	// Tolerations for runner pod
 	Tolerations []corev1.Toleration `json:"tolerations,omitempty"`
+	// Resource requirements for the runner pod
+	Resources corev1.ResourceRequirements `json:"resources,omitempty"`
 }
 
 // ExperimentList defines information about chaos experiments defined in the chaos engine
@@ -240,6 +242,8 @@ type PromProbeInputs struct {
 	Endpoint string `json:"endpoint,omitempty"`
 	// Query to get promethus metrices
 	Query string `json:"query,omitempty"`
+	// QueryPath contains filePath, which contains prometheus query
+	QueryPath string `json:"queryPath,omitempty"`
 	// Comparator check for the correctness of the probe output
 	Comparator ComparatorInfo `json:"comparator,omitempty"`
 }
@@ -282,7 +286,7 @@ type RunProperty struct {
 
 // ExperimentComponents contains ENV, Configmaps and Secrets
 type ExperimentComponents struct {
-	ENV                        []ExperimentENV               `json:"env,omitempty"`
+	ENV                        []corev1.EnvVar               `json:"env,omitempty"`
 	ConfigMaps                 []ConfigMap                   `json:"configMaps,omitempty"`
 	Secrets                    []Secret                      `json:"secrets,omitempty"`
 	ExperimentAnnotations      map[string]string             `json:"experimentannotation,omitempty"`
@@ -298,12 +302,6 @@ type ExperimentComponents struct {
 type StatusCheckTimeout struct {
 	Delay   int `json:"delay,omitempty"`
 	Timeout int `json:"timeout,omitempty"`
-}
-
-// ExperimentENV varibles to override the default values in chaosexperiment
-type ExperimentENV struct {
-	Name  string `json:"name"`
-	Value string `json:"value"`
 }
 
 // ExperimentStatuses defines information about status of individual experiments
