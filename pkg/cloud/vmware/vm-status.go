8a409a4e1a167822273c76cf89651eab5a37855a
Soumya Ghosh Dastidar <44349253+gdsoumya@users.noreply.github.com>
2021-05-13 20:55:00 +0530

    Updated DNS chaos (#357)
    * added dns spoof chaos
    
    Signed-off-by: Soumya Ghosh Dastidar <gdsoumya@gmail.com>

--- pkg/cloud/vmware/vm-status.go
+++ pkg/cloud/vmware/vm-status.go
@@ -1,31 +1,30 @@
 package vmware
 
 import (
-	"encoding/json"
-	"net/http"	
-	"io/ioutil"	
 	"crypto/tls"
+	"encoding/json"
 	experimentTypes "github.com/litmuschaos/litmus-go/pkg/vmware/vm-poweroff/types"
+	"io/ioutil"
+	"net/http"
 )
 
 //GetVMStatus gets the current status of Vcenter VM
 func GetVMStatus(experimentsDetails *experimentTypes.ExperimentDetails, cookie string) (string, error) {
 
-        type Message struct {
-                MsgValue struct {
-                        StateValue string `json:"state"`
-                } `json:"value"`
-        }
-
+	type Message struct {
+		MsgValue struct {
+			StateValue string `json:"state"`
+		} `json:"value"`
+	}
 
 	//Leverage Go's HTTP Post function to make request
-	req, err := http.NewRequest("GET","https://"+ experimentsDetails.VcenterServer +"/rest/vcenter/vm/"+ experimentsDetails.AppVmMoid +"/power/", nil)
+	req, err := http.NewRequest("GET", "https://"+experimentsDetails.VcenterServer+"/rest/vcenter/vm/"+experimentsDetails.AppVmMoid+"/power/", nil)
 	req.Header.Set("Content-Type", "application/json")
 	req.Header.Set("Cookie", cookie)
 	tr := &http.Transport{
 		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
 	}
-	
+
 	client := &http.Client{Transport: tr}
 	resp, err := client.Do(req)
 	//Handle Error
@@ -36,10 +35,10 @@ func GetVMStatus(experimentsDetails *experimentTypes.ExperimentDetails, cookie s
 	//Read the response body
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-                return "", err
-        }
-        var m1 Message
-        json.Unmarshal([]byte(body), &m1)
-        return string(m1.MsgValue.StateValue),nil
+		return "", err
+	}
+	var m1 Message
+	json.Unmarshal([]byte(body), &m1)
+	return string(m1.MsgValue.StateValue), nil
 
 }
