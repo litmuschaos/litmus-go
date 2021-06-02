efa39ec1a795eaccb9bc895cbf121c7135393143
shubhamchaudhary <shubham@chaosnative.com>
2021-06-02 18:00:26 +0530

    chore(helper): Adding statusCheckTimeouts for the helper status check
    Signed-off-by: shubhamchaudhary <shubham@chaosnative.com>

--- chaoslib/litmus/kubelet-service-kill/lib/kubelet-service-kill.go
+++ chaoslib/litmus/kubelet-service-kill/lib/kubelet-service-kill.go
@@ -83,9 +83,9 @@ func PrepareKubeletKill(experimentsDetails *experimentTypes.ExperimentDetails, c
 	}
 
 	// Wait till the completion of helper pod
-	log.Infof("[Wait]: Waiting for %vs till the completion of the helper pod", experimentsDetails.ChaosDuration+30)
+	log.Info("[Wait]: Waiting till the completion of the helper pod")
 
-	podStatus, err := status.WaitForCompletion(experimentsDetails.ChaosNamespace, appLabel, clients, experimentsDetails.ChaosDuration+30, experimentsDetails.ExperimentName)
+	podStatus, err := status.WaitForCompletion(experimentsDetails.ChaosNamespace, appLabel, clients, experimentsDetails.ChaosDuration+experimentsDetails.Timeout, experimentsDetails.ExperimentName)
 	if err != nil || podStatus == "Failed" {
 		common.DeleteHelperPodBasedOnJobCleanupPolicy(experimentsDetails.ExperimentName+"-helper-"+experimentsDetails.RunID, appLabel, chaosDetails, clients)
 		return errors.Errorf("helper pod failed, err: %v", err)
