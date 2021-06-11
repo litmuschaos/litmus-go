7ca6f8df221728319e9ca08653e8e1a37f30a0ac
Shubham Chaudhary <shubham.chaudhary@mayadata.io>
2021-06-11 11:00:46 +0530

    chore(sdk): updating sdk (#378)
    * chore(sdk): updating sdk
    
    Signed-off-by: shubhamchaudhary <shubham@chaosnative.com>
    
    Signed-off-by: Singh, Gurpreet (gs838s) <gs838s@att.com>

--- contribute/developer-guide/generate_experiment.go
+++ contribute/developer-guide/generate_experiment.go
@@ -4,22 +4,25 @@ import (
 	"bytes"
 	"io/ioutil"
 	"os"
+	"os/exec"
 	"path"
 	"path/filepath"
+	"strings"
 	"text/template"
 
 	"github.com/litmuschaos/litmus-go/contribute/developer-guide/types"
+	"github.com/litmuschaos/litmus-go/pkg/log"
 	"github.com/pkg/errors"
 	"gopkg.in/yaml.v2"
 )
 
 // GenerateExperiment generate the new/custom chaos experiment based on specified attribute file
-func GenerateExperiment(attributeFile *string, generationType string) error {
+func GenerateExperiment(attributeFile, chartType string, generationType string) error {
 
 	// Fetch all the required attributes from the given file
 	// Experiment contains all the required attributes
 	var experimentDetails types.Experiment
-	if err := GetConfig(&experimentDetails, *attributeFile); err != nil {
+	if err := getConfig(&experimentDetails, attributeFile); err != nil {
 		return err
 	}
 
@@ -28,113 +31,66 @@ func GenerateExperiment(attributeFile *string, generationType string) error {
 	if err != nil {
 		return err
 	}
+
 	// generating the parent directory name
 	// so that it can be utilise to get the relative path of all files from there
 	currDir = path.Join(currDir, "..")
 	litmusRootDir := filepath.Dir(currDir)
 
 	// creating the directory of experiment category, if not present
-	chartDIR := litmusRootDir + "/experiments/" + experimentDetails.Category
-	CreateDirectoryIfNotPresent(chartDIR)
+	categoryDIR := litmusRootDir + "/experiments/" + experimentDetails.Category
+	createDirectoryIfNotPresent(categoryDIR)
 
-	if generationType == "chart" {
-		csvFilePath := chartDIR + "/" + experimentDetails.Category + ".chartserviceversion.yaml"
-		if err = GenerateFile(experimentDetails, csvFilePath, "./templates/category_chartserviceversion.tmpl"); err != nil {
-			return err
+	// creating the directory for the experiment, if not present
+	experimentRootDIR := categoryDIR + "/" + experimentDetails.Name
+	createDirectoryIfNotPresent(experimentRootDIR)
+
+	switch strings.ToLower(generationType) {
+	case "chart":
+
+		if chartType == "category" || chartType == "all" {
+			// create category charts
+			if err := createCategoryCharts(litmusRootDir, experimentRootDIR, experimentDetails); err != nil {
+				return err
+			}
 		}
-		packageFilePath := chartDIR + "/" + experimentDetails.Category + ".package.yaml"
-		if err = GenerateFile(experimentDetails, packageFilePath, "./templates/package.tmpl"); err != nil {
-			return err
+
+		if chartType == "experiment" || chartType == "all" {
+			// create experiment charts
+			if err := createExperimentCharts(litmusRootDir, experimentRootDIR, experimentDetails); err != nil {
+				return err
+			}
 		}
-	} else if generationType == "experiment" {
 
-		// creating the directory for the experiment, if not present
-		experimentRootDIR := chartDIR + "/" + experimentDetails.Name
-		CreateDirectoryIfNotPresent(experimentRootDIR)
-		experimentDIR := experimentRootDIR + "/experiment"
-		CreateDirectoryIfNotPresent(experimentDIR)
+	default:
 
-		// creating the directory for the chaoslib, if not present
-		chaoslibRootDIR := litmusRootDir + "/chaoslib/litmus/" + experimentDetails.Name
-		CreateDirectoryIfNotPresent(chaoslibRootDIR)
-		chaoslibDIR := chaoslibRootDIR + "/lib"
-		CreateDirectoryIfNotPresent(chaoslibDIR)
-
-		// creating the directory for the environment variables file, if not present
-		experimentPKGDirectory := litmusRootDir + "/pkg/" + experimentDetails.Category
-		CreateDirectoryIfNotPresent(experimentPKGDirectory)
-		experimentPKGSubDirectory := experimentPKGDirectory + "/" + experimentDetails.Name
-		CreateDirectoryIfNotPresent(experimentPKGSubDirectory)
-		environmentDIR := experimentPKGSubDirectory + "/environment"
-		CreateDirectoryIfNotPresent(environmentDIR)
-
-		// creating the directory for the types.go file, if not present
-		typesDIR := experimentPKGSubDirectory + "/types"
-		CreateDirectoryIfNotPresent(typesDIR)
-
-		//creating the directory for test deployment
-		testDIR := experimentRootDIR + "/" + "test"
-		CreateDirectoryIfNotPresent(testDIR)
-
-		// generating the experiement.go file
-		experimentFilePath := experimentDIR + "/" + experimentDetails.Name + ".go"
-		if err = GenerateFile(experimentDetails, experimentFilePath, "./templates/experiment.tmpl"); err != nil {
+		// creating experiment dir & files
+		if err := createExperiment(experimentRootDIR, experimentDetails); err != nil {
 			return err
 		}
 
-		// generating the csv file
-		csvFilePath := experimentRootDIR + "/" + experimentDetails.Name + ".chartserviceversion.yaml"
-		if err = GenerateFile(experimentDetails, csvFilePath, "./templates/experiment_chartserviceversion.tmpl"); err != nil {
+		// creating chaoslib dir & files
+		if err := createChaosLib(litmusRootDir, experimentDetails); err != nil {
 			return err
 		}
 
-		// generating the chart file
-		chartFilePath := experimentRootDIR + "/" + "experiment.yaml"
-		GenerateFile(experimentDetails, chartFilePath, "./templates/experiment_custom_resource.tmpl")
-
-		// generating the rbac file
-		rbacFilePath := experimentRootDIR + "/" + "rbac.yaml"
-		if err = GenerateFile(experimentDetails, rbacFilePath, "./templates/experiment_rbac.tmpl"); err != nil {
+		// creating envs dir & files
+		if err := createENV(litmusRootDir, experimentDetails); err != nil {
 			return err
 		}
 
-		// generating the engine file
-		engineFilePath := experimentRootDIR + "/" + "engine.yaml"
-		if err = GenerateFile(experimentDetails, engineFilePath, "./templates/experiment_engine.tmpl"); err != nil {
+		// creating test dir & files
+		if err := createOkteto(experimentRootDIR, experimentDetails); err != nil {
 			return err
 		}
-
-		// generating the test deployment file
-		testDeploymentFilePath := testDIR + "/" + "test.yml"
-		if err = GenerateFile(experimentDetails, testDeploymentFilePath, "./templates/experiment_k8s_deployment.tmpl"); err != nil {
-			return err
-		}
-
-		// generating the chaoslib file
-		chaoslibFilePath := chaoslibDIR + "/" + experimentDetails.Name + ".go"
-		if err = GenerateFile(experimentDetails, chaoslibFilePath, "./templates/chaoslib.tmpl"); err != nil {
-			return err
-		}
-
-		// generating the environment var file
-		environmentFilePath := environmentDIR + "/" + "environment.go"
-		if err = GenerateFile(experimentDetails, environmentFilePath, "./templates/environment.tmpl"); err != nil {
-			return err
-		}
-
-		// generating the types.go file
-		typesFilePath := typesDIR + "/" + "types.go"
-		if err = GenerateFile(experimentDetails, typesFilePath, "./templates/types.tmpl"); err != nil {
-			return err
-		}
-
 	}
+
 	return nil
 }
 
-// GetConfig load data from YAML file into a structure
+// getConfig load data from YAML file into a structure
 // the object of structure can be further use to bootstrap the provided template for the experiment
-func GetConfig(experimentDetails *types.Experiment, attributeFile string) error {
+func getConfig(experimentDetails *types.Experiment, attributeFile string) error {
 
 	yamlFile, err := ioutil.ReadFile(attributeFile)
 	if err != nil {
@@ -148,8 +104,8 @@ func GetConfig(experimentDetails *types.Experiment, attributeFile string) error 
 	return nil
 }
 
-// GenerateFile bootstrap the file from the template
-func GenerateFile(experimentDetails types.Experiment, fileName string, templatePath string) error {
+// generateFile bootstrap the file from the template
+func generateFile(experimentDetails types.Experiment, fileName string, templatePath string) error {
 
 	// parse the experiment template
 	tpl, err := template.ParseFiles(templatePath)
@@ -168,12 +124,155 @@ func GenerateFile(experimentDetails types.Experiment, fileName string, templateP
 	return err
 }
 
-// CreateDirectoryIfNotPresent check for the directory and create if not present
-func CreateDirectoryIfNotPresent(path string) {
+// createDirectoryIfNotPresent check for the directory and create if not present
+func createDirectoryIfNotPresent(path string) {
 	_, err := os.Stat(path)
 
 	if os.IsNotExist(err) {
 		os.Mkdir(path, 0755)
 	}
+}
 
+// copy creates a copy of the source file at the destination
+func copy(src, dest string) error {
+	command := exec.Command("cp", src, dest)
+	var out, stderr bytes.Buffer
+	command.Stdout = &out
+	command.Stderr = &stderr
+	if err := command.Run(); err != nil {
+		log.Infof("Error String: %v", stderr.String())
+		return errors.Errorf("unable to copy file, err: %v", err)
+	}
+	return nil
+}
+
+// createExperimentFile creates the experiment file
+func createExperiment(experimentRootDIR string, experimentDetails types.Experiment) error {
+	// create the experiment directory, if not present
+	experimentDIR := experimentRootDIR + "/experiment"
+	createDirectoryIfNotPresent(experimentDIR)
+
+	// generating the experiement.go file
+	experimentFilePath := experimentDIR + "/" + experimentDetails.Name + ".go"
+	return generateFile(experimentDetails, experimentFilePath, "./templates/experiment.tmpl")
+}
+
+// createChaosLib creates the chaoslib for the experiment
+func createChaosLib(litmusRootDir string, experimentDetails types.Experiment) error {
+	// create the chaoslib directory, if not present
+	chaoslibRootDIR := litmusRootDir + "/chaoslib/litmus/" + experimentDetails.Name
+	createDirectoryIfNotPresent(chaoslibRootDIR)
+	chaoslibDIR := chaoslibRootDIR + "/lib"
+	createDirectoryIfNotPresent(chaoslibDIR)
+
+	// generating the chaoslib file
+	chaoslibFilePath := chaoslibDIR + "/" + experimentDetails.Name + ".go"
+	return generateFile(experimentDetails, chaoslibFilePath, "./templates/chaoslib.tmpl")
+}
+
+// createENV creates the env getter and setter files
+func createENV(litmusRootDir string, experimentDetails types.Experiment) error {
+	// creating the directory for the environment variables file, if not present
+	experimentPKGDirectory := litmusRootDir + "/pkg/" + experimentDetails.Category
+	createDirectoryIfNotPresent(experimentPKGDirectory)
+	experimentPKGSubDirectory := experimentPKGDirectory + "/" + experimentDetails.Name
+	createDirectoryIfNotPresent(experimentPKGSubDirectory)
+
+	// creating the directory for the environment.go file, if not present
+	environmentDIR := experimentPKGSubDirectory + "/environment"
+	createDirectoryIfNotPresent(environmentDIR)
+
+	// creating the directory for the types.go file, if not present
+	typesDIR := experimentPKGSubDirectory + "/types"
+	createDirectoryIfNotPresent(typesDIR)
+
+	// generating the environment var file
+	environmentFilePath := environmentDIR + "/" + "environment.go"
+	if err := generateFile(experimentDetails, environmentFilePath, "./templates/environment.tmpl"); err != nil {
+		return err
+	}
+
+	// generating the types.go file
+	typesFilePath := typesDIR + "/" + "types.go"
+	if err := generateFile(experimentDetails, typesFilePath, "./templates/types.tmpl"); err != nil {
+		return err
+	}
+	return nil
+}
+
+// createOkteto creates the test file for okteto
+func createOkteto(experimentRootDIR string, experimentDetails types.Experiment) error {
+	//creating the directory for test deployment
+	testDIR := experimentRootDIR + "/" + "test"
+	createDirectoryIfNotPresent(testDIR)
+
+	// generating the test deployment file
+	testDeploymentFilePath := testDIR + "/" + "test.yml"
+	return generateFile(experimentDetails, testDeploymentFilePath, "./templates/experiment_k8s_deployment.tmpl")
+}
+
+// createCategoryCharts creates the category charts
+func createCategoryCharts(litmusRootDir, experimentRootDIR string, experimentDetails types.Experiment) error {
+	// creating the chart directory, if not present
+	chartDirectory := experimentRootDIR + "/charts"
+	createDirectoryIfNotPresent(chartDirectory)
+
+	// create parent charts
+	csvFilePath := chartDirectory + "/" + experimentDetails.Category + ".chartserviceversion.yaml"
+	if err := generateFile(experimentDetails, csvFilePath, "./templates/category_chartserviceversion.tmpl"); err != nil {
+		return err
+	}
+	packageFilePath := chartDirectory + "/" + experimentDetails.Category + ".package.yaml"
+	if err := generateFile(experimentDetails, packageFilePath, "./templates/package.tmpl"); err != nil {
+		return err
+	}
+
+	// creating the icons directory, if not present
+	iconDirectory := chartDirectory + "/icons/"
+	createDirectoryIfNotPresent(iconDirectory)
+
+	// adding icon for category
+	if err := copy(litmusRootDir+types.IconPath, iconDirectory+experimentDetails.Category+".png"); err != nil {
+		return err
+	}
+	return nil
+}
+
+// createExperimentCharts creates the experiment charts
+func createExperimentCharts(litmusRootDir, experimentRootDIR string, experimentDetails types.Experiment) error {
+	// creating the chart directory, if not present
+	chartDirectory := experimentRootDIR + "/charts"
+	createDirectoryIfNotPresent(chartDirectory)
+
+	// generating the csv file
+	csvFilePath := chartDirectory + "/" + experimentDetails.Name + ".chartserviceversion.yaml"
+	if err := generateFile(experimentDetails, csvFilePath, "./templates/experiment_chartserviceversion.tmpl"); err != nil {
+		return err
+	}
+
+	// generating the chart file
+	chartFilePath := chartDirectory + "/" + "experiment.yaml"
+	generateFile(experimentDetails, chartFilePath, "./templates/experiment_custom_resource.tmpl")
+
+	// generating the rbac file
+	rbacFilePath := chartDirectory + "/" + "rbac.yaml"
+	if err := generateFile(experimentDetails, rbacFilePath, "./templates/experiment_rbac.tmpl"); err != nil {
+		return err
+	}
+
+	// generating the engine file
+	engineFilePath := chartDirectory + "/" + "engine.yaml"
+	if err := generateFile(experimentDetails, engineFilePath, "./templates/experiment_engine.tmpl"); err != nil {
+		return err
+	}
+
+	// creating the icons directory, if not present
+	iconDirectory := chartDirectory + "/icons/"
+	createDirectoryIfNotPresent(iconDirectory)
+	// adding icon for experiment
+	if err := copy(litmusRootDir+types.IconPath, iconDirectory+experimentDetails.Name+".png"); err != nil {
+		return err
+	}
+
+	return nil
 }
