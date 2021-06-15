47fd5944c063020c3ae1baf081873ad91fe5c215
Udit Gaurav <35391335+uditgaurav@users.noreply.github.com>
2021-06-15 14:58:30 +0530

    Chore(New_Exp): Add AWS SSM Chaos Experiment (#376)
    * Chore(New_Exp): Add aws ssm chaos experiment
    
    Signed-off-by: uditgaurav <udit@chaosnative.com>
    
    * Chore(New_Exp): Add AWS SSM Chaos Experiment
    
    Signed-off-by: udit <udit@chaosnative.com>
    
    * Add minor fix
    
    Signed-off-by: udit <udit@chaosnative.com>
    
    * update ENV name for cpu and default docs path
    
    Signed-off-by: udit <udit@chaosnative.com>

--- pkg/cloud/aws/ec2/ec2-instance-status.go
+++ pkg/cloud/aws/ec2/ec2-instance-status.go
@@ -4,10 +4,9 @@ import (
 	"strings"
 	"time"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ec2"
 	"github.com/litmuschaos/litmus-go/pkg/clients"
+	"github.com/litmuschaos/litmus-go/pkg/cloud/aws/common"
 	"github.com/litmuschaos/litmus-go/pkg/log"
 	"github.com/litmuschaos/litmus-go/pkg/status"
 	"github.com/litmuschaos/litmus-go/pkg/utils/retry"
@@ -21,10 +20,7 @@ func GetEC2InstanceStatus(instanceID, region string) (string, error) {
 
 	var err error
 	// Load session from shared config
-	sess := session.Must(session.NewSessionWithOptions(session.Options{
-		SharedConfigState: session.SharedConfigEnable,
-		Config:            aws.Config{Region: aws.String(region)},
-	}))
+	sess := common.GetAWSSession(region)
 
 	// Create new EC2 client
 	ec2Svc := ec2.New(sess)
@@ -44,7 +40,7 @@ func GetEC2InstanceStatus(instanceID, region string) (string, error) {
 			}
 		}
 	}
-	return "", errors.Errorf("failed to get the status of ec2 instance with instanceID %v", region)
+	return "", errors.Errorf("failed to get the status of ec2 instance with instanceID %v", instanceID)
 
 }
 
