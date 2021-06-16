cf6ae0857bc9b76bda3dc4b414b2e015ecee90a4
neelanjan00 <neelanjanmanna@gmail.com>
2021-06-16 20:05:48 +0530

    Modified function PrepareGCPTerminateByName identifier to PrepareVMTerminateByName

--- chaoslib/litmus/vm-instance-stop/lib/vm-instance-stop.go
+++ chaoslib/litmus/vm-instance-stop/lib/vm-instance-stop.go
@@ -23,8 +23,8 @@ var (
 	inject, abort chan os.Signal
 )
 
-//PrepareGCPTerminateByName contains the prepration and injection steps for the experiment
-func PrepareGCPTerminateByName(experimentsDetails *experimentTypes.ExperimentDetails, clients clients.ClientSets, resultDetails *types.ResultDetails, eventsDetails *types.EventDetails, chaosDetails *types.ChaosDetails) error {
+//PrepareVMTerminateByName contains the prepration and injection steps for the experiment
+func PrepareVMTerminateByName(experimentsDetails *experimentTypes.ExperimentDetails, clients clients.ClientSets, resultDetails *types.ResultDetails, eventsDetails *types.EventDetails, chaosDetails *types.ChaosDetails) error {
 
 	// inject channel is used to transmit signal notifications.
 	inject = make(chan os.Signal, 1)
