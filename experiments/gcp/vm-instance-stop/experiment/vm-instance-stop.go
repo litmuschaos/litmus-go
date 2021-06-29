8f9317fdfd188ae5e58c37e786872970e7525102
neelanjan00 <neelanjanmanna@gmail.com>
2021-06-29 20:02:38 +0530

    AbortWatcherWithoutExit function call removed as it was called twice in vm-instance-stop.go

--- experiments/gcp/vm-instance-stop/experiment/vm-instance-stop.go
+++ experiments/gcp/vm-instance-stop/experiment/vm-instance-stop.go
@@ -69,9 +69,6 @@ func VMInstanceStop(clients clients.ClientSets) {
 		"Sequence":        experimentsDetails.Sequence,
 	})
 
-	// Calling AbortWatcher go routine, it will continuously watch for the abort signal and generate the required events and result
-	go common.AbortWatcherWithoutExit(experimentsDetails.ExperimentName, clients, &resultDetails, &chaosDetails, &eventsDetails)
-
 	//PRE-CHAOS NODE STATUS CHECK
 	if experimentsDetails.ManagedNodegroup == "enable" {
 		activeNodeCount, err = gcp.PreChaosNodeStatusCheck(experimentsDetails.Timeout, experimentsDetails.Delay, clients)
