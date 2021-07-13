bb719bcb270b3931900bbf0372a1d456ccd73070
Neelanjan Manna <neelanjanmanna@gmail.com>
2021-07-13 09:52:51 +0530

    GCP VM Instance Stop Experiment (#388)
    * Bootstrap project created; GCP compute engine library, context library and option library added
    
    * VM instance status methods and VM instance operation methods added
    
    * Defined types.go; removed default template code for chaoslib>litmus>vm-instance-stop>lib>vm-instance-stop.go and experiments>gcp>vm-instance-stop>experiment>vm-instance-stop.go
    
    * updated function parameter names and varaible names in vm-instance-status.go
    
    * vm-instance-stop experiment chaoslib methods PrepareGCPTerminateByName, injectChaosInSerialMode, injectChaosInParallelMode, and abortWatcher added
    
    * AuxiliaryAppInfo field added in types.go and correspondingly in environment.go
    
    * Modified function PrepareGCPTerminateByName identifier to PrepareVMTerminateByName
    
    * Modified function PrepareVMTerminateByName to PrepareVMStop
    
    * VMInstanceStop experiment added
    
    * Added VMInstanceStop experiment to the main function
    
    * engine.yaml, experiment.yaml, and rbac.yaml charts updated
    
    * Corrected mount path in experiment.yaml
    
    * Functionality for fetching secret files from Kubernetes secret volume for the purpose of auth added
    
    * Updated engine.yaml chart
    
    * Initialized default value for TOTAL_CHAOS_DURATION, CHAOS_INTERVAL, and RAMP_TIME in experiment.yaml
    
    * Updated function VMInstanceStop GetENV and corresponding logs; Updated GetServiceAccountJSONFromSecret to return proper credentials
    
    * removed default comments from experiment.go
    
    * corrected yaml syntax in experiment.yaml
    
    * renamed ChaosEngine name to gcp-vm-chaos in engine.yaml
    
    * updated expirement description message in experiment.yaml
    
    * Replaced logo for GCP and VM Instance Delete experiment
    
    * terminate replaced to stop in a log in vm-instance-stop.go
    
    * Updated comments in vm-instance-stop.go
    
    * Updated comments in vm-instance-stop.go
    
    * Updated comments in vm-instance-stop.go
    
    * Removed attributes.yaml
    
    * removed the charts directory
    
    * comment added to VMInstanceStop function in vm-instance-stop.go
    
    * AbortWatcherWithoutExit function call removed as it was called twice in vm-instance-stop.go
    
    * Event generation of Result as Awaited shifted after SetResultUID function call in vm-instance-stop.go
    
    * post chaos vm instance status log moved outside the loop
    
    * test.yml updated
    
    * VM_INSTANCE_NAME and INSTANCE_ZONE replaced with their plurals
    
    * removed ActiveNodes from types.go
    
    * ManagedNodegroup replaced by AutoScalingGroup
    
    * added getFileContent function to get-credentials-json.go; implemented marshalling for creating json byte slice credentials
    
    * Modified log statement in vm-instance-status.go
    
    * Signing the commit for vm-instance-stop experiment
    
    * renamed the experiment to gcp-vm-instance-stop
    
    * Replaced the revert chaos status
    
    * Replaced PreChaosNodeStatusCheck, PostChaosActiveNodeCountCheck, and getActiveNodeCount functions to nodes.go; updated the gcp-vm-instance-stop experiment for the same.
    
    * Replaced PreChaosNodeStatusCheck, PostChaosActiveNodeCountCheck, and getActiveNodeCount functions to nodes.go; updated the ec2-terminate-by-id and ec2-terminate-by-tag.go experiments for the same.
    
    * Implemented the WaitForDuration method for waiting through the chaos interval in serial and parallel mode
    
    * Replaced Sequence with Zone in experiment log
    
    * Updated error statement
    
    * Removed redundant chaos setTarget
    
    * error message updated
    
    * Updated ramp time and chaos interval default values in environment.go
    
    * Added default ramp time of 0 in environment.go
    
    * cleaned unused dependencies
    
    * removed unused go dependencies
    
    Co-authored-by: Shubham Chaudhary <shubham.chaudhary@mayadata.io>
    Co-authored-by: Udit Gaurav <35391335+uditgaurav@users.noreply.github.com>

---
+++ pkg/gcp/gcp-vm-instance-stop/types/types.go
@@ -0,0 +1,32 @@
+package types
+
+import (
+	clientTypes "k8s.io/apimachinery/pkg/types"
+)
+
+// ExperimentDetails is for collecting all the experiment-related details
+type ExperimentDetails struct {
+	ExperimentName     string
+	EngineName         string
+	ChaosDuration      int
+	ChaosInterval      int
+	RampTime           int
+	ChaosLib           string
+	AppNS              string
+	AuxiliaryAppInfo   string
+	AppLabel           string
+	AppKind            string
+	ChaosUID           clientTypes.UID
+	InstanceID         string
+	ChaosNamespace     string
+	ChaosPodName       string
+	Timeout            int
+	Delay              int
+	VMInstanceName     string
+	GCPProjectID       string
+	InstanceZone       string
+	AutoScalingGroup   string
+	Sequence           string
+	TargetContainer    string
+	LIBImagePullPolicy string
+}
