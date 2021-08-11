a58a750bfae25e762f727a44558c1aacdda5752a
Akash Shrivastava <akash@chaosnative.com>
2021-08-11 14:14:17 +0530

    Pod experiment fixes (#429)
    * Fixed issue with pod affected percentage > 100
    
    Signed-off-by: Akash Shrivastava <akash@chaosnative.com>

--- chaoslib/litmus/pod-cpu-hog-exec/lib/pod-cpu-hog-exec.go
+++ chaoslib/litmus/pod-cpu-hog-exec/lib/pod-cpu-hog-exec.go
@@ -78,8 +78,6 @@ func experimentCPU(experimentsDetails *experimentTypes.ExperimentDetails, client
 
 // injectChaosInSerialMode stressed the cpu of all target application serially (one by one)
 func injectChaosInSerialMode(experimentsDetails *experimentTypes.ExperimentDetails, targetPodList corev1.PodList, clients clients.ClientSets, resultDetails *types.ResultDetails, eventsDetails *types.EventDetails, chaosDetails *types.ChaosDetails) error {
-	// creating err channel to recieve the error from the go routine
-	stressErr := make(chan error)
 
 	// run the probes during chaos
 	if len(resultDetails.ProbeDetails) != 0 {
@@ -92,6 +90,9 @@ func injectChaosInSerialMode(experimentsDetails *experimentTypes.ExperimentDetai
 	timeDelay := time.Duration(experimentsDetails.ChaosDuration) * time.Second
 
 	for _, pod := range targetPodList.Items {
+
+		// creating err channel to recieve the error from the go routine
+		stressErr := make(chan error)
 
 		if experimentsDetails.EngineName != "" {
 			msg := "Injecting " + experimentsDetails.ExperimentName + " chaos on " + pod.Name + " pod"
