01bc33c4c4360678963d3f3b299468b71189e2a5
YASHARTH MOHAN <yasharthsrivastava35@gmail.com>
2021-08-15 14:27:35 +0530

    Update environment.go

--- pkg/generic/network-chaos/environment/environment.go
+++ pkg/generic/network-chaos/environment/environment.go
@@ -44,21 +44,10 @@ func GetENV(experimentDetails *experimentTypes.ExperimentDetails) {
 	experimentDetails.SocketPath = common.Getenv("SOCKET_PATH", "/var/run/docker.sock")
 	experimentDetails.Sequence = common.Getenv("SEQUENCE", "parallel")
 	experimentDetails.TerminationGracePeriodSeconds, _ = strconv.Atoi(common.Getenv("TERMINATION_GRACE_PERIOD_SECONDS", ""))
-	experimentDetails.HostNetwork = Getenvbool("HOST_NETWORK", "false")
+	experimentDetails.HostNetwork = strconv.ParseBool(Getenv("HOST_NETWORK", "false"))
 	experimentDetails.VolMount = common.Getenv("VOL_MOUNT", "")
 }
 
-// Getenv fetch the env and set the default value for bool, if any
-func Getenvbool(key string, defaultValue string) bool {
-	value := os.Getenv(key)
-	var val bool
-	if value == "" || defaultValue == "false" {
-		val = false
-	} else {
-		val = true
-	}
-	return val
-}
 //InitialiseChaosVariables initialise all the global variables
 func InitialiseChaosVariables(chaosDetails *types.ChaosDetails, experimentDetails *experimentTypes.ExperimentDetails) {
 	appDetails := types.AppDetails{}
