c8fe035bddcabb3b810405be784a259133d7993f
YASHARTH MOHAN <yasharthsrivastava35@gmail.com>
2021-08-15 14:32:30 +0530

    Update environment.go

--- pkg/generic/node-restart/environment/environment.go
+++ pkg/generic/node-restart/environment/environment.go
@@ -34,20 +34,8 @@ func GetENV(experimentDetails *experimentTypes.ExperimentDetails) {
 	experimentDetails.TargetNodeIP = common.Getenv("TARGET_NODE_IP", "")
 	experimentDetails.TargetContainer = common.Getenv("TARGET_CONTAINER", "")
 	experimentDetails.NodeLabel = common.Getenv("NODE_LABEL", "")
-	experimentDetails.HostNetwork = Getenvbool("HOST_NETWORK", "false")
+	experimentDetails.HostNetwork = strconv.ParseBool(Getenv("HOST_NETWORK", "false"))
 	experimentDetails.VolMount = common.Getenv("VOL_MOUNT", "")
-}
-
-// Getenv fetch the env and set the default value for bool, if any
-func Getenvbool(key string, defaultValue string) bool {
-	value := os.Getenv(key)
-	var val bool
-	if value == "" || defaultValue == "false" {
-		val = false
-	} else {
-		val = true
-	}
-	return val
 }
 
 //InitialiseChaosVariables initialise all the global variables
