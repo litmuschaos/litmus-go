81950794f4d3845bbbe08728253f362bffe7656f
shubham chaudhary <shubham@chaosnative.com>
2021-09-24 19:59:09 +0530

    fix(bdd): fixing the bdd
    Signed-off-by: shubham chaudhary <shubham@chaosnative.com>

--- experiments/baremetal/redfish-node-restart/experiment/redfish-node-restart.go
+++ experiments/baremetal/redfish-node-restart/experiment/redfish-node-restart.go
@@ -32,11 +32,11 @@ func NodeRestart(clients clients.ClientSets) {
 	// Initialize the chaos attributes
 	types.InitialiseChaosVariables(&chaosDetails)
 
-	// Intialise Chaos Result Parameters
+	// Initialize Chaos Result Parameters
 	types.SetResultAttributes(&resultDetails, chaosDetails)
 
 	if experimentsDetails.EngineName != "" {
-		// Intialise the probe details. Bail out upon error, as we haven't entered exp business logic yet
+		// Initialize the probe details. Bail out upon error, as we haven't entered exp business logic yet
 		if err := probe.InitializeProbesInChaosResultDetails(&chaosDetails, clients, &resultDetails); err != nil {
 			log.Errorf("Unable to initialize the probes, err: %v", err)
 			return
