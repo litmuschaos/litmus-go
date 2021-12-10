bb00de6577024cdfc98e26f60cc5c62a120a6048
neelanjan00 <neelanjan@chaosnative.com>
2021-12-10 11:15:35 +0530

    added conn parameter to probe
    Signed-off-by: neelanjan00 <neelanjan@chaosnative.com>

--- chaoslib/litmus/process-kill/lib/process-kill.go
+++ chaoslib/litmus/process-kill/lib/process-kill.go
@@ -176,7 +176,7 @@ func injectChaosInParallelMode(experimentsDetails *experimentTypes.ExperimentDet
 
 		// run the probes during chaos
 		if len(resultDetails.ProbeDetails) != 0 {
-			if err = probe.RunProbes(chaosDetails, clients, resultDetails, "DuringChaos", eventsDetails, nil); err != nil {
+			if err = probe.RunProbes(chaosDetails, clients, resultDetails, "DuringChaos", eventsDetails, conn); err != nil {
 				return err
 			}
 		}
