c1bfc484543131d5fcc86b49c143e7bc36774ea3
Shubham Chaudhary <shubham.chaudhary@harness.io>
2022-10-07 16:00:30 +0530

    chore(spring-boot): spliting spring-boot-chaos experiment to separate experiments
    Signed-off-by: Shubham Chaudhary <shubham.chaudhary@harness.io>

--- experiments/spring-boot/spring-boot-latency/experiment/spring-boot-latency.go
+++ experiments/spring-boot/spring-boot-latency/experiment/spring-boot-latency.go
@@ -28,7 +28,7 @@ func Experiment(clients clients.ClientSets) {
 
 	//Fetching all the ENV passed from the runner pod
 	log.Infof("[PreReq]: Getting the ENV for the %v experiment", os.Getenv("EXPERIMENT_NAME"))
-	experimentEnv.GetENV(&experimentsDetails)
+	experimentEnv.GetENV(&experimentsDetails, "spring-boot-latency")
 
 	// Initialize the chaos attributes
 	types.InitialiseChaosVariables(&chaosDetails)
