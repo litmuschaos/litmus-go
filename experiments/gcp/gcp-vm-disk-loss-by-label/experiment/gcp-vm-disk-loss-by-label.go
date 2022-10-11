e8ec4bd0df5476f14d84d764d856038f39af8f7c
Neelanjan Manna <neelanjan.manna@harness.io>
2022-10-11 10:29:05 +0530

    fix(Experiment): Add status logs for GCP experiments (#583)
    * added status logs to GCP experiments
    
    Signed-off-by: Neelanjan Manna <neelanjan.manna@harness.io>

--- experiments/gcp/gcp-vm-disk-loss-by-label/experiment/gcp-vm-disk-loss-by-label.go
+++ experiments/gcp/gcp-vm-disk-loss-by-label/experiment/gcp-vm-disk-loss-by-label.go
@@ -103,7 +103,7 @@ func GCPVMDiskLossByLabel(clients clients.ClientSets) {
 	// Create a compute service to access the compute engine resources
 	computeService, err = gcp.GetGCPComputeService()
 	if err != nil {
-		log.Errorf("failed to obtain a gcp compute service, err: %v", err)
+		log.Errorf("Failed to obtain a gcp compute service, err: %v", err)
 		failStep := "[pre-chaos]: Failed to obtain a gcp compute service, err: " + err.Error()
 		result.RecordAfterFailure(&chaosDetails, &resultDetails, failStep, clients, &eventsDetails)
 		return
@@ -111,11 +111,13 @@ func GCPVMDiskLossByLabel(clients clients.ClientSets) {
 
 	//selecting the target instances (pre-chaos)
 	if err := gcp.SetTargetDiskVolumes(computeService, &experimentsDetails); err != nil {
-		log.Errorf("failed to get the target gcp disk volumes, err: %v", err)
+		log.Errorf("Failed to get the target gcp disk volumes, err: %v", err)
 		failStep := "[pre-chaos]: Failed to select the target disk volumes from label, err: " + err.Error()
 		result.RecordAfterFailure(&chaosDetails, &resultDetails, failStep, clients, &eventsDetails)
 		return
 	}
+
+	log.Info("[Status]: Disk volumes are attached to the VM instances (pre-chaos)")
 
 	// Including the litmus lib
 	switch experimentsDetails.ChaosLib {
@@ -142,6 +144,8 @@ func GCPVMDiskLossByLabel(clients clients.ClientSets) {
 			return
 		}
 	}
+
+	log.Info("[Status]: Disk volumes are attached to the VM instances (post-chaos)")
 
 	log.Infof("[Confirmation]: %v chaos has been injected successfully", experimentsDetails.ExperimentName)
 	resultDetails.Verdict = v1alpha1.ResultVerdictPassed
