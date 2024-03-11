ae7ebf0863359d18fb0bf25eda9c1545909a1fe5
Shubham Chaudhary <shubham.chaudhary@harness.io>
2024-03-11 21:30:08 +0530

    test: Adding fuzz testing for common util
    Signed-off-by: Shubham Chaudhary <shubham.chaudhary@harness.io>

--- pkg/utils/common/common_fuzz_test.go
+++ pkg/utils/common/common_fuzz_test.go
@@ -1,11 +1,15 @@
 package common
 
 import (
-	"github.com/stretchr/testify/assert"
 	"regexp"
 	"strconv"
 	"strings"
 	"testing"
+
+	fuzz "github.com/AdaLogics/go-fuzz-headers"
+	"github.com/litmuschaos/litmus-go/pkg/types"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func FuzzRandomInterval(f *testing.F) {
@@ -36,5 +40,114 @@ func FuzzRandomInterval(f *testing.F) {
 		} else if num > 1 && err != nil {
 			t.Errorf("Unexpected Error: %v", err)
 		}
+	})
+}
+
+func FuzzGetContainerNames(f *testing.F) {
+
+	f.Fuzz(func(t *testing.T, data []byte) {
+		fuzzConsumer := fuzz.NewConsumer(data)
+		targetStruct := &struct {
+			chaosDetails types.ChaosDetails
+		}{}
+		err := fuzzConsumer.GenerateStruct(targetStruct)
+		if err != nil {
+			return
+		}
+		names := GetContainerNames(&targetStruct.chaosDetails)
+		require.Equal(t, len(names), len(targetStruct.chaosDetails.SideCar)+1)
+	})
+}
+
+func FuzzGetSidecarVolumes(f *testing.F) {
+
+	f.Fuzz(func(t *testing.T, data []byte) {
+		fuzzConsumer := fuzz.NewConsumer(data)
+		targetStruct := &struct {
+			chaosDetails types.ChaosDetails
+		}{}
+		err := fuzzConsumer.GenerateStruct(targetStruct)
+		if err != nil {
+			return
+		}
+		volumes := GetSidecarVolumes(&targetStruct.chaosDetails)
+		var volCounts = 0
+		for _, s := range targetStruct.chaosDetails.SideCar {
+			volCounts += len(s.Secrets)
+		}
+		require.Equal(t, len(volumes), len(volumes))
+	})
+}
+
+func FuzzBuildSidecar(f *testing.F) {
+
+	f.Fuzz(func(t *testing.T, data []byte) {
+		fuzzConsumer := fuzz.NewConsumer(data)
+		targetStruct := &struct {
+			chaosDetails types.ChaosDetails
+		}{}
+		err := fuzzConsumer.GenerateStruct(targetStruct)
+		if err != nil {
+			return
+		}
+		containers := BuildSidecar(&targetStruct.chaosDetails)
+		require.Equal(t, len(containers), len(targetStruct.chaosDetails.SideCar))
+	})
+}
+
+func FuzzContains(f *testing.F) {
+	f.Fuzz(func(t *testing.T, data []byte) {
+		fuzzConsumer := fuzz.NewConsumer(data)
+		targetStruct := &struct {
+			val   string
+			slice []string
+		}{}
+		err := fuzzConsumer.GenerateStruct(targetStruct)
+		if err != nil {
+			return
+		}
+		contains := Contains(targetStruct.val, targetStruct.slice)
+		for _, s := range targetStruct.slice {
+			if s == targetStruct.val {
+				require.True(t, contains)
+				return
+			}
+		}
+		require.False(t, contains)
+	})
+}
+
+func FuzzSubStringExistsInSlice(f *testing.F) {
+	f.Fuzz(func(t *testing.T, data []byte) {
+		fuzzConsumer := fuzz.NewConsumer(data)
+		targetStruct := &struct {
+			val   string
+			slice []string
+		}{}
+		err := fuzzConsumer.GenerateStruct(targetStruct)
+		if err != nil {
+			return
+		}
+		contains := SubStringExistsInSlice(targetStruct.val, targetStruct.slice)
+		for _, s := range targetStruct.slice {
+			if strings.Contains(s, targetStruct.val) {
+				require.True(t, contains)
+				return
+			}
+		}
+		require.False(t, contains)
+	})
+}
+
+func FuzzGetRandomSequence(f *testing.F) {
+	f.Add("random")
+
+	f.Fuzz(func(t *testing.T, sequence string) {
+		val := GetRandomSequence(sequence)
+		if strings.ToLower(sequence) == "random" {
+			require.Contains(t, []string{"serial", "parallel"}, val)
+			return
+		}
+		require.Equal(t, sequence, val)
 	})
 }
