e47875b19f16831b425eb2d334d32691b6b2b086
Shubham Chaudhary <shubham.chaudhary@harness.io>
2024-03-11 22:07:38 +0530

    fix the random interval test
    Signed-off-by: Shubham Chaudhary <shubham.chaudhary@harness.io>

--- pkg/utils/common/common_fuzz_test.go
+++ pkg/utils/common/common_fuzz_test.go
@@ -28,17 +28,35 @@ func FuzzRandomInterval(f *testing.F) {
 	f.Fuzz(func(t *testing.T, interval string) {
 		re := regexp.MustCompile(`^\d+(-\d+)?$`)
 		intervals := strings.Split(interval, "-")
-		err := RandomInterval(interval)
-
-		if re.MatchString(interval) == false {
-			assert.Error(t, err, "{\"errorCode\":\"GENERIC_ERROR\",\"reason\":\"could not parse CHAOS_INTERVAL env, bad input\"}")
+		skip := false
+		if re.MatchString(interval) {
+			a, _ := strconv.Atoi(intervals[0])
+			if len(intervals) == 2 {
+				b, _ := strconv.Atoi(intervals[1])
+				if a > 5 || (b-a) > 5 || (b-a) < 1 {
+					skip = true
+				}
+			} else if len(intervals) == 1 {
+				if a > 5 || a < 1 {
+					skip = true
+				}
+			}
 		}
 
-		num, _ := strconv.Atoi(intervals[0])
-		if num < 1 && err != nil {
-			assert.Error(t, err, "{\"errorCode\":\"GENERIC_ERROR\",\"reason\":\"invalid CHAOS_INTERVAL env value, value below lower limit\"}")
-		} else if num > 1 && err != nil {
-			t.Errorf("Unexpected Error: %v", err)
+		if !skip {
+			err := RandomInterval(interval)
+			if re.MatchString(interval) == false {
+				assert.Error(t, err, "{\"errorCode\":\"GENERIC_ERROR\",\"reason\":\"could not parse CHAOS_INTERVAL env, bad input\"}")
+				return
+			}
+
+			num, _ := strconv.Atoi(intervals[0])
+			if num < 1 && err != nil {
+				assert.Error(t, err, "{\"errorCode\":\"GENERIC_ERROR\",\"reason\":\"invalid CHAOS_INTERVAL env value, value below lower limit\"}")
+				return
+			} else if num > 1 && err != nil {
+				t.Errorf("Unexpected Error: %v", err)
+			}
 		}
 	})
 }
