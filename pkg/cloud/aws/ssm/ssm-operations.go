8ab1d5712ee07e93474cfb38b21fcacbe11df054
Sami S. <25616506+ThaSami@users.noreply.github.com>
2025-03-19 04:53:42 +0300

    Update pkg/cloud/aws/ssm/ssm-operations.go
    Co-authored-by: Neelanjan Manna <neelanjanmanna@gmail.com>
    Signed-off-by: Sami Shabaneh <sami.shabaneh@careem.com>

--- pkg/cloud/aws/ssm/ssm-operations.go
+++ pkg/cloud/aws/ssm/ssm-operations.go
@@ -154,13 +154,14 @@ func CheckInstanceInformation(experimentsDetails *experimentTypes.ExperimentDeta
 
 	sesh := common.GetAWSSession(experimentsDetails.Region)
 	ssmClient := ssm.New(sesh)
-	foundInstances := make(map[string]bool)
-	input := &ssm.DescribeInstanceInformationInput{}
-
-	var err error
-	maxRetries := 5
-	maxRetryDuration := time.Second * 30
-	startTime := time.Now()
+        var (
+ 	    foundInstances make(map[string]bool)
+	    input &ssm.DescribeInstanceInformationInput{}
+	    err error
+	    maxRetries = 5
+	    maxRetryDuration = time.Second * 30
+	    startTime = time.Now()
+      )
 
 	for attempt := 0; attempt < maxRetries; attempt++ {
 		if time.Since(startTime) > maxRetryDuration {
